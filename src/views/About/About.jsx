903248f40a5754526bd685965087a4931bebe9a6
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2024-05-20 13:57:57 +0100

    Format with Prettier

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+
 import styles from "./About.module.css";
 
 function About() {
-  return (
-    <div className={styles.wrapper}>
-      <p>About</p>
-    </div>
-  );
+	return (
+		<div className={styles.wrapper}>
+			<p>About</p>
+		</div>
+	);
 }
 
 export default About;
