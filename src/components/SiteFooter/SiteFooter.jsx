2c4fc8f1682cbc6f2c3e96248c10a1d398a069e4
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2024-05-21 10:26:19 +0100

    Initial JSX

--- src/components/SiteFooter/SiteFooter.jsx
+++ src/components/SiteFooter/SiteFooter.jsx
@@ -3,7 +3,19 @@ import React from "react";
 import styles from "./SiteFooter.module.css";
 
 function SiteFooter() {
-	return <div className={styles.wrapper}>TODO Copyright</div>;
+	return (
+		<footer className={styles.wrapper}>
+			<div className={styles.termsAndCookies}>
+				<span className={`${styles.terms} ${styles.short}`}>
+					T &amp; Cs
+				</span>
+				<span className={`${styles.terms} ${styles.full}`}>
+					Terms and Conditions
+				</span>
+			</div>
+			<span className={styles.copyright}>&copy; 2024</span>
+		</footer>
+	);
 }
 
 export default SiteFooter;
