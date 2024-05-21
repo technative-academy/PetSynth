8319848a9cf79f4a3135bdd29611f232bac222e4
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2024-05-21 09:19:38 +0100

    Move SiteNav from Root.jsx to SiteHeader

--- src/components/SiteHeader/SiteHeader.jsx
+++ src/components/SiteHeader/SiteHeader.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+import SiteNav from "../SiteNav/SiteNav";
 import styles from "./SiteHeader.module.css";
 
 function SiteHeader() {
-	return <div className={styles.wrapper}>Logo</div>;
+	return (
+		<header className={styles.wrapper}>
+			<p>Logo</p>
+			<SiteNav />
+		</header>
+	);
 }
 
 export default SiteHeader;
--- src/views/Root.jsx
+++ src/views/Root.jsx
@@ -3,14 +3,12 @@ import React from "react";
 import MainContent from "../components/MainContent/MainContent";
 import SiteFooter from "../components/SiteFooter/SiteFooter";
 import SiteHeader from "../components/SiteHeader/SiteHeader";
-import SiteNav from "../components/SiteNav/SiteNav";
 import styles from "./Root.module.css";
 
 function Root() {
 	return (
 		<div className={styles.wrapper}>
 			<SiteHeader />
-			<SiteNav />
 			<MainContent />
 			<SiteFooter />
 		</div>
