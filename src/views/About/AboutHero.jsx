a3c42d8394449e0a03a6f94ea7c3554f97af82fc
Conrad G <143351775+cnrdgrgry@users.noreply.github.com>
2024-05-23 12:04:50 +0100

    Initial CSS adjustment for Primary about view, to create unified look between views.

--- src/views/About/AboutHero.jsx
+++ src/views/About/AboutHero.jsx
@@ -1,9 +1,13 @@
+import styles from "./About.module.css";
+
 // This is largely temporary until the approach with the header is decided
 export default function AboutHero() {
 	return (
-		<section>
-			<h1>About the brains</h1>
-			<p>Who we are, what we do, why we are, where we do</p>
-		</section>
+		<div className={styles.aboutHero}>
+			<h1 className={styles.aboutH1}>About the brains</h1>
+			<h3 className={styles.aboutH3}>
+				Who we are, what we do, why we are, where we do
+			</h3>
+		</div>
 	);
 }
