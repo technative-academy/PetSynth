a5d5411981897a4d7e64596575d02c9720a78798
sbennett <susannah-bennett@hotmail.com>
2024-05-24 12:02:34 +0100

    Tweak layout

--- src/views/Shop/Product.jsx
+++ src/views/Shop/Product.jsx
@@ -3,20 +3,16 @@ import styles from "./Shop.module.css";
 // Individual Product (pet) Component:
 function Product({ image, title, description, stars, price }) {
 	return (
-		<div className={styles.productcard}>
-			<li className={styles.productitem}>
-				<img
-					src={`https://project-2.technative.dev.f90.co.uk${image}`}
-					className={styles.productImg}
-				/>
-				<p className={styles.animalname}>{title}</p>
-				<p class Name={styles.petdesc}>
-					{description}
-				</p>
-				<p>{stars}</p>
-				<p>{price}</p>
-			</li>
-		</div>
+		<li className={styles.productcard}>
+			<img
+				src={`https://project-2.technative.dev.f90.co.uk${image}`}
+				className={styles.productImg}
+			/>
+			<p className={styles.animalname}>{title}</p>
+			<p className={styles.petdetails}>{description}</p>
+			<p className={styles.petdetails}>{stars} ⭐</p>
+			<p className={styles.petdetails}>£{price}</p>
+		</li>
 	);
 }
 
